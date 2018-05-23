262d11b619b3b30e5098b25e2f7d05b74707d144
shivashriganesh <spm1215@gmail.com>
2018-05-23 02:04:02 -0500

    Remove testing stuff

--- src/client/ClientRunner.java
+++ src/client/ClientRunner.java
@@ -1,26 +1,28 @@
 package client;
 
+import javax.swing.*;
+
 public class ClientRunner {
     public static void main(String[] args) {
         // Ask for IP to connect to
-//        boolean isIPValid = false;
-//        String IP = null;
-//
-//        do {
-//            try {
-//                IP = JOptionPane.showInputDialog(null, "Which IP to connect to?");
-//                if (!IP.matches("[0-9]{1,4}.[0-9]{1,4}.[0-9]{1,4}.[0-9]{1,4}"))
-//                    throw new NumberFormatException();
-//            } catch (NumberFormatException e) {
-//                JOptionPane.showMessageDialog(null, "Please enter a valid IP");
-//                continue;
-//            }
-//            isIPValid = true;
-//        } while (!isIPValid);
-//
-//        System.out.println(IP);
-//
-//        new ClientInterface(IP, 1234);
-        new ClientInterface("127.0.0.1", 1234);
+        boolean isIPValid = false;
+        String IP = null;
+
+        do {
+            try {
+                IP = JOptionPane.showInputDialog(null, "Which IP to connect to?");
+                if (!IP.matches("[0-9]{1,4}.[0-9]{1,4}.[0-9]{1,4}.[0-9]{1,4}"))
+                    throw new NumberFormatException();
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Please enter a valid IP");
+                continue;
+            }
+            isIPValid = true;
+        } while (!isIPValid);
+
+        System.out.println(IP);
+
+        new ClientInterface(IP, 1234);
+//        new ClientInterface("127.0.0.1", 1234);
     }
 }
